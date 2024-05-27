69d7f96d71983d250b01de8cc8b6710ba9b5b32a
repoocuts <84625989+repoocuts@users.noreply.github.com>
2024-05-27 08:04:11 +0100

    Added typewriter effect

--- app/frontend/components/blurb_component_controller.js
+++ app/frontend/components/blurb_component_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller {
 	static targets = ["paragraph"];
 
 	connect() {
-		console.log('Blurb controller connected', this.element);
+		console.log('Blurb controller connected');
 		this.startTypingEffect();
 	}
 
@@ -14,13 +14,17 @@ export default class extends Controller {
 
 		const typeParagraph = () => {
 			if (index < paragraphs.length) {
+				const textLength = paragraphs[index].textContent.length;
+				const typingDuration = textLength * 0.02;
+				const blinkingDuration = textLength / 2;
+				paragraphs[index].style.animation = `typing ${typingDuration}s steps(${textLength}, end) forwards, blinking ${blinkingDuration}s step-end infinite`;
 				paragraphs[index].classList.add("typing-active");
 				paragraphs[index].addEventListener('animationend', () => {
 					paragraphs[index].classList.remove("typing-active");
 					paragraphs[index].classList.add("typing-done");
 					index++;
-					setTimeout(typeParagraph, 500);  // Delay before typing the next line
-				});
+					setTimeout(typeParagraph, 500);
+				}, { once: true });
 			}
 		};
 
--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,16 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class HelloController extends Controller {
   connect() {
-    console.log('hellooooo controller connected');
-    this.element.addEventListener('mouseenter', this.showContent.bind(this));
-    this.element.addEventListener('mouseleave', this.hideContent.bind(this));
-  }
-
-  showContent() {
-    this.element.classList.add('hovered');
-  }
-
-  hideContent() {
-    this.element.classList.remove('hovered');
+    console.log('hello controller connected', this.element);
   }
 }
